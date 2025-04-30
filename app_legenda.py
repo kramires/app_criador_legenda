2a4506fa588671b72d66d7490a3cb55d42086a95
Klayton Ramires <ramires03@hotmail.com>
2025-04-30 19:40:59 -0300

    erro

--- app_legenda.py
+++ app_legenda.py
@@ -5,7 +5,6 @@ import os
 from pathlib import Path
 from argostranslate import translate
 import torch
-torch.classes  # força o carregamento correto
 
 def format_time(seconds):
     hours = int(seconds // 3600)
